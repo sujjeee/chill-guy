cf04116f4afdfe1f361d7c12c34f4dd92e3444b9
Suraj <sujjeee@outlook.com>
2024-12-06 15:17:51 +0530

    add toggle animations

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -26,6 +26,8 @@ import { CheckIcon } from "lucide-react"
 import { cn } from "@/lib/utils"
 import { selectedTextPropertiesProps } from "@/hooks/use-fabric"
 
+import { AnimatePresence, motion } from "framer-motion"
+
 interface ToolbarProps {
   setBackgroundImage: (imageUrl: string) => Promise<Canvas | null>
   addText: () => void
@@ -151,112 +153,137 @@ export function Toolbar({
             <span className="tooltiptext">Text</span>
             <Icons.text className="size-4" />
           </Button>
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant="outline"
-                size={"icon"}
-                className="rounded-full hover:animate-jelly tooltip shrink-0"
-              >
-                <span className="tooltiptext">Font</span>
-                <Icons.font className="size-4" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent
-              align="start"
-              className="max-w-[200px] w-full p-0 h-[250px] rounded-lg"
-            >
-              <Command>
-                <CommandInput placeholder="Search font family..." />
-                <CommandList className="hide_scrollbar">
-                  <CommandEmpty>No font family found.</CommandEmpty>
-                  <CommandGroup heading="Recommended">
-                    {/* <ScrollArea className="h-[250px] w-full"> */}
-                    {recommendedFonts.map((fontName) => {
-                      return (
-                        <CommandItem
-                          key={fontName}
-                          value={fontName}
-                          className={cn("cursor-pointer")}
-                          onSelect={(currentValue) => {
-                            changeFontFamily(currentValue)
-                          }}
-                        >
-                          <span
-                            style={{ fontFamily: `'${fontName}', sans-serif` }}
-                          >
-                            {fontName}
-                          </span>
-                          <CheckIcon
-                            className={cn(
-                              "ml-auto size-4",
-                              fontName === selectedTextProperties.fontFamily
-                                ? "opacity-100"
-                                : "opacity-0",
-                            )}
-                          />
-                        </CommandItem>
-                      )
-                    })}
-                  </CommandGroup>
-                  <CommandGroup heading="Others">
-                    {/* <ScrollArea className="h-[250px] w-full"> */}
-                    {otherFonts.map((fontName) => {
-                      return (
-                        <CommandItem
-                          key={fontName}
-                          value={fontName}
-                          className={cn("cursor-pointer")}
-                          onSelect={(currentValue) => {
-                            changeFontFamily(currentValue)
-                          }}
-                        >
-                          <span
-                            style={{ fontFamily: `'${fontName}', sans-serif` }}
-                          >
-                            {fontName}
-                          </span>
-                          <CheckIcon
-                            className={cn(
-                              "ml-auto size-4",
-                              fontName === selectedTextProperties.fontFamily
-                                ? "opacity-100"
-                                : "opacity-0",
-                            )}
-                          />
-                        </CommandItem>
-                      )
-                    })}
-                    {/* </ScrollArea> */}
-                  </CommandGroup>
-                </CommandList>
-              </Command>
-            </PopoverContent>
-          </Popover>
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant="outline"
-                size={"icon"}
-                className="rounded-full hover:animate-jelly tooltip shrink-0 "
-                style={{ backgroundColor: selectedTextProperties.fontColor }}
-              >
-                <span className="tooltiptext">Text Color</span>
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent
-              className="mt-3 w-fit p-0 bg-transparent rounded-lg"
-              align="start"
-            >
-              <HexColorPicker
-                className="border-none"
-                color={selectedTextProperties.fontColor}
-                onChange={(color: string) => {
-                  return changeTextColor(color)
+          <AnimatePresence>
+            {selectedTextProperties.isTextSelected && (
+              <motion.div
+                initial={{ opacity: 0, x: -20 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -20, transition: { duration: 0.09 } }}
+                transition={{
+                  duration: 0.1,
+                  stiffness: 900,
+                  type: "spring",
+                  damping: 50,
                 }}
-              />
-            </PopoverContent>
-          </Popover>
+                className="flex items-center space-x-2"
+              >
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant="outline"
+                      size={"icon"}
+                      className="rounded-full hover:animate-jelly tooltip shrink-0"
+                    >
+                      <span className="tooltiptext">Font Family</span>
+                      <Icons.font className="size-4" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent
+                    align="start"
+                    className="max-w-[200px] w-full p-0 h-[250px] rounded-lg"
+                  >
+                    <Command>
+                      <CommandInput placeholder="Search font family..." />
+                      <CommandList className="hide_scrollbar">
+                        <CommandEmpty>No font family found.</CommandEmpty>
+                        <CommandGroup heading="Recommended">
+                          {/* <ScrollArea className="h-[250px] w-full"> */}
+                          {recommendedFonts.map((fontName) => {
+                            return (
+                              <CommandItem
+                                key={fontName}
+                                value={fontName}
+                                className={cn("cursor-pointer")}
+                                onSelect={(currentValue) => {
+                                  changeFontFamily(currentValue)
+                                }}
+                              >
+                                <span
+                                  style={{
+                                    fontFamily: `'${fontName}', sans-serif`,
+                                  }}
+                                >
+                                  {fontName}
+                                </span>
+                                <CheckIcon
+                                  className={cn(
+                                    "ml-auto size-4",
+                                    fontName ===
+                                      selectedTextProperties.fontFamily
+                                      ? "opacity-100"
+                                      : "opacity-0",
+                                  )}
+                                />
+                              </CommandItem>
+                            )
+                          })}
+                        </CommandGroup>
+                        <CommandGroup heading="Others">
+                          {/* <ScrollArea className="h-[250px] w-full"> */}
+                          {otherFonts.map((fontName) => {
+                            return (
+                              <CommandItem
+                                key={fontName}
+                                value={fontName}
+                                className={cn("cursor-pointer")}
+                                onSelect={(currentValue) => {
+                                  changeFontFamily(currentValue)
+                                }}
+                              >
+                                <span
+                                  style={{
+                                    fontFamily: `'${fontName}', sans-serif`,
+                                  }}
+                                >
+                                  {fontName}
+                                </span>
+                                <CheckIcon
+                                  className={cn(
+                                    "ml-auto size-4",
+                                    fontName ===
+                                      selectedTextProperties.fontFamily
+                                      ? "opacity-100"
+                                      : "opacity-0",
+                                  )}
+                                />
+                              </CommandItem>
+                            )
+                          })}
+                          {/* </ScrollArea> */}
+                        </CommandGroup>
+                      </CommandList>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant="outline"
+                      size={"icon"}
+                      className="rounded-full hover:animate-jelly tooltip shrink-0 "
+                      style={{
+                        backgroundColor: selectedTextProperties.fontColor,
+                      }}
+                    >
+                      <span className="tooltiptext">Text Color</span>
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent
+                    className="mt-3 w-fit p-0 bg-transparent rounded-lg"
+                    align="start"
+                  >
+                    <HexColorPicker
+                      className="border-none"
+                      color={selectedTextProperties.fontColor}
+                      onChange={(color: string) => {
+                        return changeTextColor(color)
+                      }}
+                    />
+                  </PopoverContent>
+                </Popover>
+              </motion.div>
+            )}
+          </AnimatePresence>
           <div className="h-5">
             <div className="mx-1.5 h-full w-px bg-[#e5e5e5]"></div>
           </div>
