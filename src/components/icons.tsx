7d7960961bc70c513dcc71bb3af0796f9cb86e45
Manish Prajapati <manishprajapat41@gmail.com>
2024-12-09 23:43:27 +0530

    Add PDF generation

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -141,4 +141,31 @@ export const Icons = {
       </g>
     </svg>
   ),
+  pdf: ({ ...props }: IconProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none" fillRule="evenodd">
+        <rect width="24" height="24" fill="#FFFFFF" rx="4" />
+
+        <text
+          x="12"
+          y="16"
+          fill="#E74C3C"
+          fontFamily="Arial, sans-serif"
+          fontWeight="bold"
+          fontSize="12"
+          textAnchor="middle"
+        >
+          PDF
+        </text>
+      </g>
+    </svg>
+
+  )
+
 }
